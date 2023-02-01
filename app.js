a15f989e792e4ef9e0b2fc2684ab409d242be141
ldubravica <luka.dubravica@gmail.com>
2023-02-01 16:44:29 +0100

    Messages & Turns

--- app.js
+++ app.js
@@ -2,19 +2,18 @@ const express = require("express");
 const http = require("http");
 const websocket = require("ws");
 
-let currentGames = [];
+let currentGames = new Map();
 let waitingPlayers = [];
-let playerID = 0; // try: removing or renaming to sessionID which stays across both splash and game screen
-let gameID = 0;
+let playerID = 99;
+let gameID = 99;
 
 class Game {
 	constructor() {
 		this.id = -1;
-		this.player1 = -1; // set to ws
-		this.player2 = -1; // set to ws
+		this.players = [];
+		this.turn = 0;
 		this.board = [[1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0],[0,0,0,0,0,0,0,0],
 					  [0,0,0,0,0,0,0,0],[0,2,0,2,0,2,0,2],[2,0,2,0,2,0,2,0],[0,2,0,2,0,2,0,2]];
-		this.turn = 1; // when set to 0, game is loading
 	}
 }
 
@@ -28,38 +27,25 @@ const wss = new websocket.Server({ server });
 
 wss.on("connection", function(ws) {
 
-	// WARM UP
-	console.log("\n[SER] server ready");
-
-	// SET UP CLIENT
-	ws.send(`playerid: ${(playerID % 2) + 1}`);
-	waitingPlayers.unshift(ws);
-	playerID++;
-	
-	// SET UP GAME
     ws.on("message", function incoming(message) {
 		console.log(`[CLI] ${message}`);
-		if(message == "screen:splash") {
-			playerID--;
-			waitingPlayers.shift();
-			console.log(`[SER] player ${playerID} deregistered due to splash`);
-			ws.send(currentGames.length);
-		}
-		else if (message.startsWith("registered:")) {
+		if (message == "screen:splash") {
+			ws.send(currentGames.size);
+		} else if (message == "screen:game") {
+			ws.send(`pid:${++playerID}`);
+			waitingPlayers.unshift(ws);
 			setUpGame();
-		}
-		// UPDATE BOARD
-		else if (message.startsWith("board:")) { // board:3:1:1010...0202
+		} else if (message.startsWith("board:")) { // board:gid:pid:[[...]]
 			updateBoard(message);
-		}
-		// EXIT
-		else if (message.startsWith("exit:")) { // exit:3:2
-			exitGame(message, ws);
+		} else if (message.startsWith("turn:")) { // turn:gid:pid
+			updateTurn(message);
+		} else if (message.startsWith("exit:")) { // exit:gid:pid:won/left
+			exitGame(message, null);
 		}
 	});
 
 	ws.on("close", function() {
-		exitGame("exit:0:0", ws);
+		exitGame("exit:-1:-1:left", ws);
 	});
 
 });
@@ -70,64 +56,64 @@ function setUpGame() {
 	if (waitingPlayers.length < 2) return;
 
 	const newGame = new Game();
-	newGame.id = gameID++; // not tested: ++
-	newGame.player1 = waitingPlayers.pop();
-	newGame.player2 = waitingPlayers.pop();
-	currentGames.push(newGame);
+	newGame.id = ++gameID;
+	newGame.players = [waitingPlayers.pop(), waitingPlayers.pop()];
+	currentGames.set(newGame.id, newGame);
 
-	try{
-		newGame.player1.send(`gameid:bit:${newGame.id}`);
-		newGame.player2.send(`gameid:usd:${newGame.id}`);
+	try {
+		newGame.players[0].send(`gid:bit:${newGame.id}`);
+		newGame.players[1].send(`gid:usd:${newGame.id}`);
 	}
 	catch {
-		exitGame("exit:0");
+		currentGames.delete(newGame.id);
 	}
 }
 
-function updateBoard(message) {  // board:3:1:1010...0202
+function updateBoard(message) { // board:gid:pid:[[...]]
 	const input = message.split(":");
-	const currGameID = input[1];
-	const currPlayerID = input[2];
-	const nextPlayerID = (currPlayerID == 1) ? 2 : 1;
-	const currBoard = JSON.parse(input[3]);
-	let pos = -1;
+	const currGameID = parseInt(input[1]);
+	
+	const currGame = currentGames.get(currGameID);
+	if (currGame == null) return;
 
-	for (let i = 0; i < currentGames.length; i++) {
-		if (currentGames[i].id == currGameID) {
-			pos = i;
-			i = currentGames.length;
-		}
-	}
+	currGame.board = JSON.parse(input[3])
+	currentGames.set(currGameID, currGame);
 
-	currentGames[pos].board = currBoard;
-	currentGames[pos].turn = nextPlayerID;
-	currentGames[pos][`player${nextPlayerID}`].send(`board:${JSON.stringify(currentGames[pos].board)}`);
+	currGame.players[(currGame.turn + 1) % 2].send(`board:${JSON.stringify(currGame.board)}`);
 }
 
-function exitGame(message, ws) {
-	let input = message.split(":");
+function updateTurn(message) { // turn:gid:pid
+	const input = message.split(":");
+	const currGameID = parseInt(input[1]);
 	
-	for (let i = 0; i < currentGames.length; i++) {
-		if (currentGames[i].id == input[1]) {
-			if (input[2] == 0) {
-				input[2] = ws == currentGames[i].player1 ? 1 : 2; 
+	const currGame = currentGames.get(currGameID);
+	currGame.turn = (currGame.turn + 1) % 2;
+	currentGames.set(currGameID, currGame);
+
+	currGame.players[currGame.turn].send(`turn:${currGame.turn}`);
+}
+
+function exitGame(message, ws) { // exit:gid:pid:won/left
+	const input = message.split(":");
+	let currGameID = parseInt(input[1]);
+
+	if (currGameID == -1) {
+		for (const pair of currentGames.entries()) {
+			const key = pair[0];
+			const value = pair[1];
+
+			if (value.players.includes(ws)) {
+				currGameID = key;
+				break;
 			}
-
-			const contact = currentGames[i][`player${input[2] == 1 ? 2 : 1}`];
-			if (contact != -1) contact.send(`exit:${input[1]}`);
-
-			currentGames[i].player1 = -1;
-			currentGames[i].player2 = -1;
-
-			if (i == currentGames.length - 1) {
-				currentGames.pop();
-			}
-			else {
-				currentGames[i] = currentGames[currentGames.length - 1];
-				currentGames.pop();
-			}
-
-			return;
 		}
-	}
+	} 
+
+	const currGame = currentGames.get(currGameID);
+	if (currGame == null) return;
+
+	const nextPlayerStatus = input[3] == "won" ? "lost" : "won";
+	currGame.players[(currGame.turn + 1) % 2].send(`exit:${nextPlayerStatus}`);
+
+	currentGames.delete(currGameID);
 }
--- public/javascripts/game-control.js
+++ public/javascripts/game-control.js
@@ -2,12 +2,11 @@ const socket = new WebSocket("ws://localhost:3000");
 
 let playerID = -1;
 let gameID = -1;
-let hasWon = false;
-let hasLost = false;
 let board = [[1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0],[0,0,0,0,0,0,0,0],
 			 [0,0,0,0,0,0,0,0],[0,2,0,2,0,2,0,2],[2,0,2,0,2,0,2,0],[0,2,0,2,0,2,0,2]];
 
 let playerTurn;
+let playerAte;
 let friendlyPieces;
 let enemyPieces;
 let diffMoveY;
@@ -40,27 +39,30 @@ function movePiece(clickedID) {
 	const currY = parseInt(clickedID.substring(1,2));
 	const currX = parseInt(clickedID.substring(2,3));
 
-	if (!playerTurn) {
+	if (!playerTurn) { // if not player's turn
 		notify("Not your turn!", true);
-	} else if (!holding && friendlyPieces.includes(board[currY][currX])) {
+	} else if (!holding && friendlyPieces.includes(board[currY][currX])) { // if the first selected tile is a friendly piece
 		oldX = currX;
 		oldY = currY;
 		holding = true;
+		playerAte = false;
 		document.getElementById(`d${oldY}${oldX}`).style.background = "#006600";
-	} else if (holding && validMove(oldX, oldY, currX, currY)) {
+	} else if (holding && validMove(oldX, oldY, currX, currY)) { // if the second selected tile is a valid move
+		// update and generate new board
 		board[currY][currX] = currY == endLine ? friendlyPieces[1] : board[oldY][oldX];
 		board[oldY][oldX] = 0;
+		document.getElementById(`d${oldY}${oldX}`).style.background = "#2b002c";
 		setUpBoard();
 
-		document.getElementById(`d${oldY}${oldX}`).style.background = "#2b002c";
 		holding = false;
-		playerTurn = false;
+		socket.send(`board:${gameID}:${playerID}:${JSON.stringify(board)}`);
+		playerTurn = playerAte;
+		if (!playerTurn) socket.send(`turn:${gameID}:${playerID}`)
 
-		if (gameOver()) {
-			socket.send(`exit:${gameID}:${playerID}`);
-			notify(`You ${hasWon ? "won" : "lost"}!`, false);
-		} else {
-			socket.send(`board:${gameID}:${playerID}:${JSON.stringify(board)}`);
+		// notify the server and the user if the game is over
+		if (hasUserWon()) {
+			socket.send(`exit:${gameID}:${playerID}:won`);
+			notify(`You won!`, false);
 		}
 	} else {
 		document.getElementById(`d${oldY}${oldX}`).style.background = "#2b002c";
@@ -71,8 +73,9 @@ function movePiece(clickedID) {
 
 // VALIDITY OF THE MOVE
 
-function validMove(startX, startY, endX, endY) { // ADD RECURSIVE CALL FOR EATING or allow user to move his piece again only if he is eating again, implement later w/ networking
-	if (board[endY][endX] != 0) return false;
+function validMove(startX, startY, endX, endY) {
+	if (board[endY][endX] != 0)
+		return false;
 	if (board[startY][startX] == friendlyPieces[0])
 		return validMoveForward(startX, startY, endX, endY) || validEat(startX, startY, endX, endY);
 	if (board[startY][startX] == friendlyPieces[1])
@@ -80,20 +83,24 @@ function validMove(startX, startY, endX, endY) { // ADD RECURSIVE CALL FOR EATIN
 }
 
 function validMoveForward(startX, startY, endX, endY) {
-	return endY - startY == diffMoveY && Math.abs(endX - startX) == 1;
+	const allowedY = endY - startY == diffMoveY;
+	const allowedX = Math.abs(endX - startX) == 1;
+	return allowedY && allowedX;
 }
 
 function validEat(startX, startY, endX, endY) {
 	const enemyX = (startX + endX) / 2;
 	const enemyY = startY + diffEnemyY;
+	let allowedMove = false;
 
 	if (endY - startY != diffEatY || Math.abs(endX - startX) != 2) return false;
 	if (enemyPieces.includes(board[enemyY][enemyX])) {
 		board[enemyY][enemyX] = 0;
-		return true;
+		allowedMove = true;
 	}
 	
-	return false;
+	playerAte = allowedMove;
+	return allowedMove;
 }
 
 function validQueenMove(startX, startY, endX, endY) {
@@ -116,25 +123,24 @@ function validQueenMove(startX, startY, endX, endY) {
 		}
 		if (friendlyPieces.includes(board[startY][startX])) return false;
 	}
-	if (foundEnemy) board[enemyY][enemyX] = 0;
+
+	if (foundEnemy) {
+		board[enemyY][enemyX] = 0;
+		playerAte = true;
+	}
 	
 	return true;
 }
 
 // TOOLS
 
-function gameOver() {
-	hasWon = true;
-	hasLost = true;
-
+function hasUserWon() {
 	for (let i = 0; i < 8; i++) {
 		for (let j = 0; j < 8; j++) {
-			if (friendlyPieces.includes(board[j][i])) hasLost = false;
-			else if (enemyPieces.includes(board[j][i])) hasWon = false;
+			if (enemyPieces.includes(board[j][i])) return false;
 		}
 	}
-	
-	return hasWon || hasLost;
+	return true;
 }
 
 function notify(msg, timeout) {
@@ -155,33 +161,38 @@ function setupPlayer(bitPlayer) {
 	endLine = bitPlayer ? 7 : 0;
 }
 
+function leaveGame() {
+	socket.send(`exit:${gameID}:${playerID}:left`);
+}
+
 // WEB SOCKET
 
-socket.onmessage = function(event){
+socket.onopen = function() {
+	socket.send("screen:game");
+};
+
+socket.onmessage = function(event) {
 	const input = event.data.split(":");
-	if (input[0] == "playerid") {
-		playerID = input.pop();
-		socket.send(`registered: player${playerID}`);
+	if (input[0] == "pid") {
+		playerID = input[1];
+		socket.send(`registered:pid:${playerID}`);
 		notify("waiting for opponent", false);
-	} else if (input[0] == "gameid") {
+	} else if (input[0] == "gid") {
 		setupPlayer(input[1] == "bit");
 		gameID = input[2];
-		socket.send(`game ${gameID}: registered player ${playerID}`);
+		socket.send(`registered:gid:${gameID}:pid:${playerID}`);
 		setUpBoard();
 		notify(`You are ${playerTurn ? "first, bitcoin!" : "second, dollar!"}`, true);
 	} else if (input[0] == "board") {
-		board = JSON.parse(input.pop());
-		playerTurn = true;
-		socket.send(`game ${gameID}: board received`);
+		board = JSON.parse(input[1]);
+		socket.send(`received:${gameID}:${playerID}`);
 		setUpBoard();
+	} else if (input[0] == "turn") {
+		playerTurn = true;
 	} else if (input[0] == "exit") {
 		playerTurn = false;
-		notify("You won!", false);
+		notify(`You ${input[1]}!`, false);
 	} else {
 		socket.send(`Invalid message sent to player ${playerID}: ${input}`);
 	}
-};
-
-socket.onopen = function(){
-	socket.send("screen:game");
 };
